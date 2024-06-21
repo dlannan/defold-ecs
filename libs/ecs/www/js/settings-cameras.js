bf9f7f9bc9b5de67d81d519054cdc1f65db6e989
David Lannan <david.lannan@caytrix.com>
2024-06-21 10:55:21 +0930

    camera updates now working. camera page fairly complete.

--- libs/ecs/www/js/settings-cameras.js
+++ libs/ecs/www/js/settings-cameras.js
@@ -6,12 +6,12 @@ function formatVec( param )  {
   return vec;
 }
 
-function UpdateCameraProps( index )
+function UpdateCameraProps( )
 {
   if(g_cameras.cameras.length > 0) {
 
     let html_props = "";
-    var cam = g_cameras.cameras[index];
+    var cam = g_cameras.cameras[g_current_camera];
     console.log(cam);
     // By default set the first camera when starting (if there are any)
     for (let [key, item] of Object.entries(cam))   {
@@ -33,27 +33,34 @@ $(document).ready(function() {
     g_cameras = cameras;
     
     cameras.cameras.forEach( function(cam, index, arr) {
+      console.log(index);
       html_cameras = html_cameras + '<div class="row">';
       html_cameras = html_cameras + '<div class="col-auto text-right update-meta pr-0">';
       html_cameras = html_cameras + '<i class="ik ik-camera bg-blue update-icon"></i>';
       html_cameras = html_cameras + '</div>';
       html_cameras = html_cameras + '<div class="col pl-5">';
-      html_cameras = html_cameras + '<a href="#!"><h6>' + cam.name + '</h6></a>';
+      html_cameras = html_cameras + '<div class="camera-select" data="'+ index + '"><a href="#!"><h6>' + cam.name + '</h6></a></div>';
       html_cameras = html_cameras + '</div>';
       html_cameras = html_cameras + '<div class="col-2 pl-5">';
-      html_cameras = html_cameras + '<a href="#!" style="color: white;padding-top:6px;"><h4><i name="'+ cam.name + '" data="' + cam.go + '" class="ik ik-aperture camera-select"></i></h4></a>';
+      html_cameras = html_cameras + '<a href="#!" style="color: white;padding-top:6px;"><h4><i name="'+ cam.name + '" data="' + cam.go + '" class="ik ik-aperture camera-set"></i></h4></a>';
       html_cameras = html_cameras + '</div></div>';
     });
     
     $("#cameras-list").html(html_cameras);
-    UpdateCameraProps(0);
+    UpdateCameraProps();
   });
 
   $(".camera-select").on("click", function(e) {
+    console.log(this);
+    let index = $(this).attr("data");
+    g_current_camera = Number(index);
+    UpdateCameraProps();
+  });
+
+  $(".camera-set").on("click", function(e) {
   
-      let go = $(e.target).attr("data");
-      let name = $(e.target).attr("name");
-      console.log(go);
+      let go = $(this).attr("data");
+      let name = $(this).attr("name");
       postData("/world/camera/enable", { go: go, name: name }, function() {});
   });
 });
